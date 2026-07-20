3d6f2cabb37b1832a236e0700c92ddccfc825b65
Matin Soleymani <matinsoleymni@gmail.com>
2026-07-21 00:43:28 +0330

    fix

--- app/Telegram/Keyboards/TelegramReporterMenuKeyboard.php
+++ app/Telegram/Keyboards/TelegramReporterMenuKeyboard.php
@@ -17,7 +17,7 @@ class TelegramReporterMenuKeyboard
                 InlineKeyboardButton::make('ریپورت کانال', callback_data: 'telegram_report_channel', style: 'danger', icon_custom_emoji_id: '4918203446202467778')
             )
             ->addRow(
-                InlineKeyboardButton::make('ریپورت پست', callback_data: 'telegram_report_post', style: 'danger', icon_custom_emoji_id: '6226448624843231576')
+                InlineKeyboardButton::make('ریپورت پست', callback_data: 'check_countdown', style: 'danger', icon_custom_emoji_id: '6226448624843231576')
             )
             ->addRow(
                 InlineKeyboardButton::make('بازگشت', callback_data: 'reporter_menu', style: 'danger', icon_custom_emoji_id: '5352759161945867747')
